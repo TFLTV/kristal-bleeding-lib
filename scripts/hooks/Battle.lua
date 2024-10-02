29a61f9ac2b69951f84926f7d7e76d7326e544bf
TFLTV <105890618+TFLTV@users.noreply.github.com>
2024-10-02 14:44:21 -0500

    Update Battle.lua

--- scripts/hooks/Battle.lua
+++ scripts/hooks/Battle.lua
@@ -1,21 +1,12 @@
 ---@class Battle
 ---
----@field bleed_timer               number                         Bleed Time. Bleed when time. Time when bleed. BLEED.
+---@field bleed_timer               number                         How much time there is before you bleed again, in seconds.
 ---@field bleeders                  PartyBattler[]                 Party Members who bleed.
 ---@field graze_heal                number                         How much grazing heals you.
 ---@field bleed_amount              number                         How much health you lose per bleed. (Maybe can be a float?? idk)
--- @field party                     PartyBattler[]                 A table of all the `PartyBattler`s in the current battle -- for reference
+---@field do_bleed                  bool                           If bleeding is toggled. (Also controls graze heal)
 
 local Battle, super = Class("Battle")
-
---[[function Battle:onStateChange(old,new)
-    super.onStateChange(self,old,new)
-    if new == "DEFENDING" then
-        self.timer:every(4/30,function()
-
-        end)
-    end
-end]]
 
 function Battle:init()
 	super.init(self)
@@ -26,7 +17,7 @@ end
 function Battle:update()
 	super.update(self)
 
-    local bleeders = Game.bleeders or (self.bleeders or {self.party[1]}) -- There, if you wanna change it, it's easy. -- self.bleeder uses PartyBattlers and numbers, Game.bleeders only uses numbers
+    local bleeders = self.bleeders or (Game.bleeders or {self.party[1]}) -- There, if you wanna change it, it's easy. -- self.bleeder uses PartyBattlers and numbers, Game.bleeders only uses numbers
     local bleed = (self.do_bleed or Game.do_bleed) ~= false
     local bleed_amount = self.bleed_amount or (Game.bleed_amount or 1)
     
@@ -34,14 +25,29 @@ function Battle:update()
         
         if bullet.tp ~= 0 and bullet:collidesWith(self.soul.graze_collider) and bullet.grazed == false and self.soul.inv_timer == 0 then -- Bullet tp is not 0 and bullet is grazing and bullet is not already been grazed and soul does not have inv time
             
-            local graze_heal = bullet.graze_heal or (bullet.attacker.graze_heal or (self.soul.graze_heal or (self.graze_heal or (Game.bleed_graze_heal or 10+Utils.random(0,2,1) )))) -- HELL
+            local graze_heal =
+            bullet.graze_heal or 
+
+            ((bullet.wave and bullet.wave.graze_heal) or 
+
+            ((bullet.attacker and bullet.attacker.graze_heal) or 
+
+            (self.soul.graze_heal or 
+
+            (self.graze_heal or 
+
+            (Game.bleed_graze_heal or 
+
+            10+Utils.random(0,2,1) ))))) -- HELL
+            -- The lower it is on the list, the less priority it has (Higher values on the list 'overwrite' the lower ones if they exist, the bottom value will always exist).
+
             -- It seems that in the original mod, the graze heal is determined by bullet/enemy so uhh set it.
 
             for _,bleeder in ipairs(bleeders) do
                 if not bleed then break end
                 if type(bleeder) == "number" then bleeder = self.party[bleeder] end
 
-                if not bleeder.is_down then                
+                if not bleeder.is_down then
 
                     bleeder.chara:setHealth( bleeder.chara:getHealth() + graze_heal )
 
