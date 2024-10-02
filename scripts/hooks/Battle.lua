0088c3924d73f52e08b46939e64129b7f50221db
TFLTV <105890618+TFLTV@users.noreply.github.com>
2024-10-02 10:01:57 -0500

    Update Battle.lua

--- scripts/hooks/Battle.lua
+++ scripts/hooks/Battle.lua
@@ -20,28 +20,26 @@ end]]
 function Battle:init()
 	super.init(self)
     self.bleed_timer = 4/30 -- Every four frames
+    self.bleeders = {self.party[1]}
 end
 
 function Battle:update()
 	super.update(self)
 
-    local bleeders = self.bleeders or {self.party[1]} -- There, if you wanna change it, it's easy.
-    local bleed = Game.do_bleed
-    local bleed_amount = self.bleed_amount or 1
+    local bleeders = Game.bleeders or (self.bleeders or {self.party[1]}) -- There, if you wanna change it, it's easy. -- self.bleeder uses PartyBattlers and numbers, Game.bleeders only uses numbers
+    local bleed = (self.do_bleed or Game.do_bleed) ~= false
+    local bleed_amount = self.bleed_amount or (Game.bleed_amount or 1)
     
     for _,bullet in ipairs(Game.stage:getObjects(Bullet)) do
         
         if bullet.tp ~= 0 and bullet:collidesWith(self.soul.graze_collider) and bullet.grazed == false and self.soul.inv_timer == 0 then -- Bullet tp is not 0 and bullet is grazing and bullet is not already been grazed and soul does not have inv time
             
-            local graze_heal = bullet.graze_heal or (self.soul.graze_heal or (self.graze_heal or 10+Utils.random(0,2,1) ))
-            if bullet.graze_heal_priority then
-                
-            end
-            -- It seems that in the original mod, the graze heal is determined by encounter so uhh set it.
-            --[[if self.soul then
-                graze_heal = self.soul.graze_heal or graze_heal
-            end]]
+            local graze_heal = bullet.graze_heal or (bullet.attacker.graze_heal or (self.soul.graze_heal or (self.graze_heal or (Game.bleed_graze_heal or 10+Utils.random(0,2,1) )))) -- HELL
+            -- It seems that in the original mod, the graze heal is determined by bullet/enemy so uhh set it.
+
             for _,bleeder in ipairs(bleeders) do
+                if not bleed then break end
+                if type(bleeder) == "number" then bleeder = self.party[bleeder] end
 
                 if not bleeder.is_down then                
 
@@ -60,6 +58,8 @@ function Battle:update()
         if self.bleed_timer <= 0 then
 
             for _,bleeder in ipairs(bleeders) do
+                if not bleed then break end
+                if type(bleeder) == "number" then bleeder = self.party[bleeder] end
                 bleeder:removeHealth(bleed_amount)
             end
 
@@ -69,6 +69,8 @@ function Battle:update()
     end
 
     for _,bleeder in ipairs(bleeders) do
+        if not bleed then break end
+        if type(bleeder) == "number" then bleeder = self.party[bleeder] end
         if bleeder.is_down then
             Game:gameOver(self:getSoulLocation())
         end
